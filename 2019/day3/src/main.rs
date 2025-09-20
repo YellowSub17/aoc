eebeae23a1fcb1513eb98db07edff27e30101e98
Patrick Adams <patrick_aust@hotmail.com>
2025-09-21 01:27:04 +0200

    super fast intersection

--- 2019/day3/src/main.rs
+++ 2019/day3/src/main.rs
@@ -4,114 +4,216 @@
 
 use std::fs::read_to_string;
 
-
+#[derive(Clone, Debug,Copy)]
 struct Point {
     x: i32,
     y: i32,
 }
 
+#[derive(Clone, Debug,Copy)]
 struct Segment {
     p1: Point,
-    p2: Point
+    p2: Point,
 }
 
+#[derive(Clone, Debug)]
 struct Instruction {
-    card: char,
-    mag: usize,
+    card: String,
+    mag: i32,
 }
 
 
 fn main() {
     println!("Hello, world!");
-    let (w1, w2) = read_wires();
-    
-    let pos1 = gen_pos(w1);
-    let pos2 = gen_pos(w2);
-    let x = cmp_pos(pos1, pos2);
-    
-    //let w1corners = gen_corners(w1);
-    /*let w2corners = gen_corners(w2);*/
-
-    /*let out = cmp_corners(w1corners, w2corners);*/
-}
-
-
-fn split_inst(inst: String) -> (String, usize){
-    let mut chars = inst.chars();
-    let card = chars.next().unwrap().to_string();
-    let mag: usize = chars.collect::<String>().parse().unwrap();
-    (card, mag)
-}
-
-fn read_wires() -> (Vec<String>, Vec<String>) {
     let input = read_to_string("./input.prod").unwrap();
-
     let mut lines = input.lines();
 
-    let w1: Vec<String> = lines.next()
+    let w1: Vec<Instruction> = lines.next()
                              .unwrap()
                              .split(",")
                              .map(|s| s.to_string())
+                             .map(split_inst)
                              .collect();
 
-    let w2: Vec<String> = lines.next()
+    let w2: Vec<Instruction> = lines.next()
                              .unwrap()
                              .split(",")
                              .map(|s| s.to_string())
+                             .map(split_inst)
                              .collect();
+    
+    //let pos1 = gen_pos(w1);
+    //let pos2 = gen_pos(w2);
+    //let x = cmp_pos(pos1, pos2);
+    
+    let w1corners = gen_corners(w1);
+    let w2corners = gen_corners(w2);
+    let x = find_wire_intersections(w1corners,w2corners);
 
-    (w1, w2)
+    println!("{:?}", x);
+
+
+
+
+
+
+
+    /*let x = calc_seg_intersection(*/
+        /*Segment{p1:Point{x:2,y:6},p2:Point{x:10,y:6}},*/
+        /*Segment{p1:Point{x:4,y:2},p2:Point{x:100,y:434}},*/
+    /*);*/
+    /*println!("{:?}", x);*/
+
+
+}
+
+
+fn split_inst(inst: String) -> Instruction {
+    let mut chars = inst.chars();
+    let card = chars.next().unwrap().to_string();
+    let mag: i32 = chars.collect::<String>().parse().unwrap();
+    Instruction{card:card , mag:mag}
+}
+
+
+
+fn calc_seg_intersection( s1: Segment, s2: Segment) -> Option<Point>{
+
+    let s1minx = s1.p1.x.min(s1.p2.x);
+    let s1maxx = s1.p1.x.max(s1.p2.x);
+
+    let s2minx = s2.p1.x.min(s2.p2.x);
+    let s2maxx = s2.p1.x.max(s2.p2.x);
+    let s1miny = s1.p1.y.min(s1.p2.y);
+    let s1maxy = s1.p1.y.max(s1.p2.y);
+
+    let s2miny = s2.p1.y.min(s2.p2.y);
+    let s2maxy = s2.p1.y.max(s2.p2.y);
+
+
+    if s1.p1.x ==s1.p2.x && s2.p1.x == s2.p2.x { //both horizontal
+        return None
+    } else if s1.p1.y ==s1.p2.y && s2.p1.y == s2.p2.y { //both vertical
+        return None
+    }
+
+    else if s1.p1.y == s1.p2.y && s2.p1.x == s2.p2.x { // s1 horzi, s2 verti
+        let s1s2inter = Point{x: s2.p1.x, y: s1.p1.y};
+        if (s1minx..s1maxx).contains(&s1s2inter.x) && (s2miny..s2maxy).contains(&s1s2inter.y) {
+            return Some(s1s2inter)
+        }
+    }
+
+    else if s1.p1.x == s1.p2.x && s2.p1.y == s2.p2.y { // s2 horzi, s1 verti
+        let s1s2inter = Point{x: s1.p1.x, y: s2.p1.y};
+        if (s2minx..s2maxx).contains(&s1s2inter.x) && (s1miny..s1maxy).contains(&s1s2inter.y) {
+            return Some(s1s2inter)
+        }
+    }
+    None
+
+
+
+}
+
+fn find_wire_intersections( w1corners: Vec<Point>, w2corners: Vec<Point>) -> Vec<Point> {
+    
+    let mut out: Vec< Point> = vec![];
+    out.push(Point{x:0, y:0});
+
+    for w1window in w1corners.windows(2) {
+        let s1: Segment = Segment{ p1: w1window[0], p2:w1window[1]};
+        for w2window in w2corners.windows(2) {
+
+            let s2: Segment = Segment{ p1: w2window[0], p2:w2window[1]};
+            //println!("{:?} {:?}", s1, s2);
+            match calc_seg_intersection(s1,s2){
+                Some(val) => out.push(val),
+                None => ()
+            }
+
+        }
+
+    }
+    out
 
 }
 
 
 
+fn gen_corners(w: Vec<Instruction>) -> Vec<Point>{
 
-fn cmp_pos( pos1: Vec<(i32, i32)>, pos2: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut current_x: i32 = 0;
+    let mut current_y: i32 = 0;
 
-    let mut out: Vec< (i32, i32)> = vec![];
+    let mut out: Vec<Point> = vec![];
+
+    out.push( Point{x:0, y:0} );
+
+    for inst in w {
+        if inst.card == "U" {
+            current_y += inst.mag as i32;
+        } else if inst.card == "D" {
+            current_y -= inst.mag as i32;
+        } else if inst.card == "L" {
+            current_x -= inst.mag as i32;
+        } else if inst.card == "R" {
+            current_x += inst.mag as i32;
+        }
+        out.push( Point{x:current_x, y:current_y} );
+    }
+
+    out
+}
+
+
+
+
+
+fn cmp_pos( pos1: Vec<Point>, pos2: Vec<Point>) -> Vec<Point> {
+
+    let mut out: Vec<Point> = vec![];
 
     for p1 in pos1.clone() {
         for p2 in pos2.clone() {
-            if p1.0==p2.0 && p1.1==p2.1{
-                out.push( (p1.0, p1.1) );
-                println!("found! {} {}", p1.0, p1.1);
+            if p1.x==p2.x && p1.y==p2.y{
+                out.push( Point{x:p1.x, y:p1.y} );
+                println!("found! {} {}", p1.x, p1.y);
             }
         }
     }
     out
 }
 
-fn gen_pos(w: Vec<String> ) -> Vec<(i32, i32)> {
+fn gen_pos(w: Vec<Instruction> ) -> Vec<Point> {
 
     let mut current_x: i32 = 0;
     let mut current_y: i32 = 0;
 
-    let mut out: Vec< (i32, i32)> = vec![];
-    out.push( (0, 0) );
+    let mut out: Vec< Point> = vec![];
+    out.push( Point{x: current_x,y: current_y} );
 
-    for (card, mag) in w.into_iter().map( |inst| split_inst(inst)){
-        //println!("{}", card);
+    for instr in w {
 
-        if card == "U" {
-            for _ in 1..mag+1 {
+        if instr.card == "U" {
+            for _ in 1..instr.mag+1 {
                 current_y += 1;
-                out.push( (current_x, current_y) );
+                out.push( Point{x: current_x,y: current_y} );
             }
-        } else if card == "D" {
-            for _ in 1..mag+1 {
+        } else if instr.card == "D" {
+            for _ in 1..instr.mag+1 {
                 current_y -= 1;
-                out.push( (current_x, current_y) );
+                out.push( Point{x: current_x,y: current_y} );
             }
-        } else if card == "L" {
-            for _ in 1..mag+1 {
+        } else if instr.card == "L" {
+            for _ in 1..instr.mag+1 {
                 current_x -= 1;
-                out.push( (current_x, current_y) );
+                out.push( Point{x: current_x,y: current_y} );
             }
-        } else if card == "R" {
-            for _ in 1..mag+1 {
+        } else if instr.card == "R" {
+            for _ in 1..instr.mag+1 {
                 current_x += 1;
-                out.push( (current_x, current_y) );
+                out.push( Point{x: current_x,y: current_y} );
             }
         } else {
             println!("AAAARRRGGGHHH");
@@ -126,52 +228,51 @@ fn gen_pos(w: Vec<String> ) -> Vec<(i32, i32)> {
 
 
 
-fn cmp_corners( w1corners: Vec<(i32, i32)>, w2corners: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+/*fn cmp_corners( w1corners: Vec<(i32, i32)>, w2corners: Vec<(i32, i32)>) -> Vec<(i32, i32)> {*/
     
-    let mut out: Vec< (i32, i32)> = vec![];
+    /*let mut out: Vec< (i32, i32)> = vec![];*/
 
-    for w1window in w1corners.windows(2) {
-        let (w1p1, w1p2) = (&w1window[0], &w1window[1]);
-        for w2window in w2corners.windows(2) {
-            let (w2p1, w2p2) = (&w2window[0], &w2window[1]);
-            //println!("{} {} {} {} ", w1p1.0, w1p2.0, w2p1.0, w2p2.0)
+    /*for w1window in w1corners.windows(2) {*/
+        /*let (w1p1, w1p2) = (&w1window[0], &w1window[1]);*/
+        /*for w2window in w2corners.windows(2) {*/
+            /*let (w2p1, w2p2) = (&w2window[0], &w2window[1]);*/
+            /*//println!("{} {} {} {} ", w1p1.0, w1p2.0, w2p1.0, w2p2.0)*/
 
-        }
+        /*}*/
 
-    }
-    out
-
-
-
-}
+    /*}*/
+    /*out*/
 
 
 
+/*}*/
 
 
-fn gen_corners(w: Vec<String>) -> Vec<(i32, i32)>{
 
-    let mut current_x: i32 = 0;
-    let mut current_y: i32 = 0;
 
-    let mut out: Vec< (i32, i32)> = vec![];
+/*fn gen_corners(w: Vec<String>) -> Vec<(i32, i32)>{*/
 
-    out.push( (0, 0) );
+    /*let mut current_x: i32 = 0;*/
+    /*let mut current_y: i32 = 0;*/
 
-    for (card, mag) in w.into_iter().map( |inst| split_inst(inst)){
+    /*let mut out: Vec< (i32, i32)> = vec![];*/
 
-        if card == "U" {
-            current_y += mag as i32;
-        } else if card == "D" {
-            current_y -= mag as i32;
-        } else if card == "L" {
-            current_x -= mag as i32;
-        } else if card == "R" {
-            current_x += mag as i32;
-        }
-        out.push( (current_x, current_y) );
-    }
+    /*out.push( (0, 0) );*/
 
-    out
-}
+    /*for (card, mag) in w.into_iter().map( |inst| split_inst(inst)){*/
+
+        /*if card == "U" {*/
+            /*current_y += mag as i32;*/
+        /*} else if card == "D" {*/
+            /*current_y -= mag as i32;*/
+        /*} else if card == "L" {*/
+            /*current_x -= mag as i32;*/
+        /*} else if card == "R" {*/
+            /*current_x += mag as i32;*/
+        /*}*/
+        /*out.push( (current_x, current_y) );*/
+    /*}*/
+
+    /*out*/
+/*}*/
 
