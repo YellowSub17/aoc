5f5afa205a58aa8e0879463e214b253a7fb381e7
Patrick Adams <patrick_aust@hotmail.com>
2025-12-08 10:42:07 +0100

    2025d8p1p2

--- 2025/day8/main2.py
+++ 2025/day8/main2.py
@@ -10,11 +10,9 @@ import math
 
 
 
-fname = './input.test'
-nconnections = 10
-
 fname = './input.prod'
-nconnections = 1000
+
+# fname = './input.prod'
 
 points = np.loadtxt(fname, delimiter=',')
 point_ids = np.arange(len(points))
@@ -35,10 +33,11 @@ for i, (id1, id2) in enumerate(pair_ids):
 pre_calc_d = pre_calc_d[pre_calc_d[:, 2].argsort()]
 # print(pre_calc_d)
 
-closest_pairs = pre_calc_d[:nconnections, :-1]
+
+closest_pairs = pre_calc_d[:, :-1]
 print('####')
-for id1, id2 in closest_pairs:
-    print(id1, id2, '\t', points[int(id1), :],'\t', points[int(id2),:])
+# for id1, id2 in closest_pairs:
+    # print(id1, id2, '\t', points[int(id1), :],'\t', points[int(id2),:])
 print('####')
 
 circuits = []
@@ -46,10 +45,10 @@ circuits = []
 
 
 # count=0
-for id1, id2 in closest_pairs:
+for ixx, (id1, id2) in enumerate(closest_pairs):
 
-    print('Attempting to connect points:')
-    print(int(id1), int(id2), '\t', points[int(id1), :],'\t', points[int(id2),:])
+    # print('Attempting to connect points:')
+    # print(int(id1), int(id2), '\t', points[int(id1), :],'\t', points[int(id2),:])
 
     circ_id1 = None
     circ_id2 = None
@@ -81,37 +80,52 @@ for id1, id2 in closest_pairs:
         # visited_junctions.add(id1)
         # visited_junctions.add(id2)
 
-    
     print(f'Circuit ids were: {circ_id1}, {circ_id2}')
     if circ_id1 is not None and circ_id2 is not None:
         print('Both circ ids not none...')
         if circ_id1 != circ_id2:
             print('And not equal to eachother!')
             print(f'need to combine sets: {circuits[circ_id1]}, {circuits[circ_id2]}')
-            # combo_circ = 
             circuits[circ_id2] = circuits[circ_id2].union(circuits[circ_id1])
             circuits.pop(circ_id1)
 
 
-    print('Circuits:')
-    for c in circuits:
-        print(c)
+    if len(circuits ) == 1 and len(circuits[0])==point_ids.shape[0]:
+        print('%%%%%')
+        print(ixx, points[int(id1)], points[int(id2)])
+        print(points[int(id1)][0], points[int(id2)][0])
+        print(points[int(id1)][0]* points[int(id2)][0])
+
+        # if circ_id1==0 and circ_id2 is None:
+        # print('BREAK')
+        # breakpoint()
+        break
+
+
+    print('#Circuits:', len(circuits))
+    print()
+    # for c in circuits:
+        # print(c)
 
     # breakpoint()
 
 
 
 
-circuits.sort(key = lambda s: len(s))
-for circuit in circuits:
-    print(circuit)
+# circuits.sort(key = lambda s: len(s))
+# for circuit in circuits:
+    # print(circuit)
 
-print(len(circuits[-1])*len(circuits[-2])*len(circuits[-3]))
+# print(len(circuits[-1])*len(circuits[-2])*len(circuits[-3]))
 
 #4590 wrong
 
 
+#394761294 too low
+#1109399334 too low
 
+#1895205564 too low
+#2347225200
 
 
 
