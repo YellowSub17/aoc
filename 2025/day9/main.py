f4219b4e5ae759323832290fb02e22f6947fb85c
Patrick Adams <patrick_aust@hotmail.com>
2025-12-10 01:52:49 +0100

    2025d9p2 need to add 1 to ymax xmax for 1x n tile cases

--- 2025/day9/main.py
+++ 2025/day9/main.py
@@ -2,93 +2,230 @@
 import numpy as np
 import itertools
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
+import bisect
 
-fname = './input.test'
+fname = './input.prod'
+
+plasma = colormaps['plasma']
+
+# Take colors at regular intervals spanning the colormap.
+# colors = cmap(np.linspace(0, 1, n_lines))
 
 
 
 
 
 points = np.loadtxt(fname, delimiter=',', dtype=int)
+points_loop = np.concat((points, [points[0,:]]), axis=0)
+
+
+
+class LineSeg:
+
+    def __init__(self, xs, ys):
+        self.xs = np.array(xs)
+        self.ys = np.array(ys)
+
+        self.vertical = self.xs[0]==self.xs[1]
+        self.horizontal = not self.vertical
+
+        if self.vertical:
+            self.start = self.ys[0]
+            self.end = self.ys[1]
+            self.c = self.xs[0]
+        else:
+            self.start = self.xs[0]
+            self.end = self.xs[1]
+            self.c = self.ys[0]
+
+    def __str__(self):
+
+        if self.horizontal:
+            s =f'H line segment at y={self.ys[0]} from x={self.xs}'
+        else:
+            s =f'V line segment at x={self.xs[0]} from y={self.ys}'
+        return s
+
+    def plot(self, **kwargs):
+        plt.plot(self.xs, self.ys, **kwargs)
+
+
+
+
+
+
+
+
+x_pts = points[:,0]
+y_pts = points[:,1]
+
+line_segs = []
+
+for i, (x1y1, x2y2) in enumerate(itertools.pairwise(points_loop)):
+    line_segs.append( LineSeg([x1y1[0], x2y2[0]], [x1y1[1], x2y2[1]]) )
+
+horizontal_line_segs = line_segs[1::2]
+vertical_line_segs = line_segs[::2]
+
+# horizontal_line_segs = line_sprod::2]
+# vertical_line_segs = line_segs[1::2]
+
+horizontal_line_segs.sort(key = lambda ls: ls.c)
+vertical_line_segs.sort(key = lambda ls: ls.c)
+
+
+
 pairs = list(itertools.combinations(points, 2))
+print(len(pairs))
+
+
+count =0
+max_area =0
+
+pair_start =4163
+# for i, (p1, p2) in enumerate(pairs[pair_start::1]):
+for i, (p1, p2) in enumerate([ [ (97323, 56370), (3121, 59426)] ]):
+
+
+    print(i, end='\r')
+
+    xmin = min(p1[0], p2[0])
+    xmax = max(p1[0], p2[0])
+
+    ymin = min(p1[1], p2[1])
+    ymax = max(p1[1], p2[1])
+    x = xmax-xmin
+    y = ymax-ymin
+    # print(xmin, xmax, ymin, ymax)
+
+
+    vcrop = vertical_line_segs[:]
+    vcrop = list(filter(lambda ls:  ls.ys.min()<ymax and ls.ys.max()>ymin, vcrop))
+    vcrop = list(filter(lambda ls: ls.c>=xmin and ls.c<=xmax, vcrop))
+
+    hcrop = horizontal_line_segs[:]
+    hcrop = list(filter(lambda ls:  ls.xs.min()<xmax and ls.xs.max()>xmin, hcrop))
+    hcrop = list(filter(lambda ls: ls.c>=ymin and ls.c<=ymax, hcrop))
+
+
+#     plt.figure()
+    # for ls in line_segs:
+        # ls.plot(color=(0.0, 0.0, 0.0))
+    # for ls in vcrop:
+        # ls.plot(color='r', linewidth=1)
+    # for ls in hcrop:
+        # ls.plot(color='r', linewidth=1)
+    # plt.plot( [p1[0], p2[0], p2[0], p1[0], p1[0]], [p1[1], p1[1], p2[1], p2[1], p1[1]], color='m', ls='dashed')
+    # plt.plot(p1[0], p1[1], 'g.', ms=25)
+    # plt.plot(p2[0], p2[1], 'm.', ms=25)
+    # plt.show()
+
+
+    # plt.show()
+
+    if len(hcrop)>0 or len(vcrop)>0:
+        continue
+    print()
+    print()
+    count +=1
+    area = x*y
+
+    if area> max_area:
+        max_area=area
+        print('new max area!', p1, p2)
+        print(max_area)
+        plt.figure()
+        for ls in line_segs:
+            ls.plot(color=(0.0, 0.0, 0.0))
+        for ls in vcrop:
+            ls.plot(color='r', linewidth=1)
+        for ls in hcrop:
+            ls.plot(color='r', linewidth=1)
+        plt.plot( [p1[0], p2[0], p2[0], p1[0], p1[0]], [p1[1], p1[1], p2[1], p2[1], p1[1]], color='m', ls='dashed')
+        plt.plot(p1[0], p1[1], 'g.', ms=25)
+        plt.plot(p2[0], p2[1], 'm.', ms=25)
+        plt.show()
+    print()
+    print()
+print('###')
+print(max_area)
+
+
+#287881312 too low
 
 
 
-ny = points[:,1].max()+1
-nx = points[:,0].max()+1
-
-
-
-# for p1, p2 in pairs:
-
-    # total_floor = np.zeros( (ny, nx) )
-    # print(p1, p2)
-    # xmin = min(p1[0], p2[0])
-    # xmax = max(p1[0], p2[0])
-    # ymin = min(p1[1], p2[1])
-    # ymax = max(p1[1], p2[1])
-    # x = xmax-xmin+1
-    # y = ymax-ymin+1
-
-    # total_floor[ymin:ymin+y, xmin:xmin+x] = 1
-    # print(total_floor)
-
-    # floor = np.zeros( (y+1, x+1) )
-    # print(floor)
+    # print(vcrop_start, vcrop_end, hcrop_start, hcrop_end)
 
     # plt.figure()
-    # plt.title(f'{p1=}, {p2=} first')
-    # plt.imshow(total_floor, aspect='equal', extent=[0, nx, ny, 0] )
-
-    # plt.figure()
-    # plt.imshow(floor, aspect='equal', extent=[0, x, y, 0])
-    # # plt.show()
-    # # break
+    # for ls in line_segs:
+        # ls.plot(color=(0.0, 0.0, 0.0))
 
 
-    # for p1p, p2p in pairs:
-        # print('\t', p1p, p2p)
-        # xminp = min(p1p[0], p2p[0])
-        # if xminp > xmax:
-            # print('\tOUT OUT BOUNDS 0')
-            # break
-        # xmaxp = max(p1p[0], p2p[0])
-        # if xmaxp < xmin:
-            # print('\tOUT OUT BOUNDS 1')
-            # break
-        # yminp = min(p1p[1], p2p[1])
-        # if yminp > ymax:
-            # print('\tOUT OUT BOUNDS 2')
-            # break
-        # ymaxp = max(p1p[1], p2p[1])
-        # if ymaxp < ymin:
-            # print('\tOUT OUT BOUNDS 3')
-            # break
+    # for ls in vcrop:
+        # ls.plot(color='r', linewidth=1)
 
-        # xp = xmaxp -xminp+1
-        # yp = ymaxp -yminp+1
+    # for ls in hcrop:
+        # ls.plot(color='r', linewidth=1)
 
 
-        # xstart = max(0, xminp -xmin)
-        # xend = min(x, xminp -xmin)
+#     plt.plot( [p1[0], p2[0], p2[0], p1[0], p1[0]], [p1[1], p1[1], p2[1], p2[1], p1[1]], color='m', ls='dashed')
+    # plt.plot(p1[0], p1[1], 'g.', ms=25)
+    # plt.plot(p2[0], p2[1], 'm.', ms=25)
 
 
-        # ystart = max(0, yminp -ymin)
-        # yend = min(y, yminp -ymin)
-
-        # floor[xstart:xend, ystart:yend] = 1
+    # plt.show()
 
 
-        # total_floor[xminp:xminp+xp, yminp:ymin+yp] = 1
+print(count)
 
 
-        # plt.figure()
-        # plt.imshow(total_floor, extent=[0, nx, ny, 0] )
 
-        # plt.figure()
-        # plt.imshow(floor, extent=[0, x, y, 0])
+    # bisect.bisect_left
 
-        # plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# plt.plot(50000, 75000, 'g.')
+
+
+
+
+
+
+
+
+
+# lhs = bisect.bisect_left(vertical_line_segs, 50000, key=lambda ls: ls.c)
+# print(lhs, vertical_line_segs[lhs])
+
+
+# lhs = bisect.bisect_right(horizontal_line_segs, 75000, key=lambda ls: ls.ys[0])
+# horizontal_line_segs[lhs].plot(color='k')
+
+# rhs = bisect.bisect_right(horizontal_line_segs, 75000, key=lambda ls: ls.ys[0])
+# horizontal_line_segs[rhs].plot(color='r')
+
+# print(lhs, horizontal_line_segs[lhs])
+# print(rhs, horizontal_line_segs[rhs])
+  # for i, ls in enumerate(horizontal_line_segs):
+        # ls.plot(color = colormaps['viridis'](np.linspace(0, 1, len(horizontal_line_segs))[i]))
+    # for i, ls in enumerate(vertical_line_segs):
+        # ls.plot(color = colormaps['viridis'](np.linspace(0, 1, len(vertical_line_segs))[i]))
+
 
 
 
@@ -111,6 +248,8 @@ nx = points[:,0].max()+1
 
 
 # ####part 1
+
+# pairs = list(itertools.combinations(points, 2))
 
 # # def find_area(p1, p2):
 
@@ -138,41 +277,6 @@ nx = points[:,0].max()+1
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # directions = [0,1,2,3] ## +x, -y, -x, +y
-# current_direction = -1
-# directions  =  np.ones(points.shape[0])*-1
-# for i,  p1, p2 in enumerate(zip(points[:-1], points[1:])):
-    
-    # if p1[0] == p2[0] and p1[1]> p2[1]:
-        # current_direction = 2
-    # elif p1[0] == p2[0] and p1[1]> p2[1]:
-
-    # elif p1[0] == p2[0] and p1[1]> p2[1]:
-
-    # else:    # p1[0] == p2[0] and p1[1]> p2[1]:
 
 
 
